04a2bcab8daeeb7441a4827867f384d05057ef0d
Cannabis2013 <MH2012@outlook.dk>
2022-03-15 23:26:44 +0100

    Initial commit

---
+++ src/ConsolePrintFormatter/FilledTextBox/Helpers/DecorateLines.java
@@ -0,0 +1,26 @@
+package ConsolePrintFormatter.FilledTextBox.Helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DecorateLines {
+    private String reset = (char) 27 + "[m";
+
+    private String formatLine(String str, String csiCode){
+        return  csiCode + str + reset;
+    }
+
+    private List<String> formatLines(List<String> strings, String csiCode){
+        var formattedLines = strings.stream().map(l -> formatLine(l,csiCode));
+        var list = formattedLines.collect(Collectors.toCollection(ArrayList::new));
+        return list;
+    }
+
+    BuildColorCSI _buildColorCode = new BuildColorCSI();
+
+    public List<String> fill(List<String> lines, String csiCode){
+        var filledLines = formatLines(lines,csiCode);
+        return filledLines;
+    }
+}
---
+++ src/ConsolePrintFormatter/Utils/LineTools/StringSplitter.java
@@ -0,0 +1,48 @@
+package ConsolePrintFormatter.Utils.LineTools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class StringSplitter {
+    int _length = 0;
+    int _currentIndex = 0;
+    List<String> _lines = new ArrayList<>();
+    String _str;
+
+    private boolean updateString(int indexOfSpace){
+        try {
+            _str = _str.substring(indexOfSpace + 1);
+            return true;
+        } catch (IndexOutOfBoundsException  e){
+            return false;
+        }
+    }
+
+    private boolean evaluateLineLength(int indexOfSpace){
+        var indexOfLastCharacter = indexOfSpace - 1;
+        String line;
+        if(indexOfLastCharacter < 0)
+            line = " ";
+        else
+            line = _str.substring(0,indexOfLastCharacter + 1);
+        _lines.add(line);
+        if(line.length() > _length)
+            _length = line.length();
+        return updateString(indexOfSpace);
+    }
+
+    public List<String> split(String str){
+        _str = str;
+        boolean hasNext = true;
+        while (hasNext){
+            var indexOfSpace = _str.indexOf('\n');
+            if(indexOfSpace != -1)
+                hasNext = evaluateLineLength(indexOfSpace);
+            else
+                hasNext = false;
+        }
+        if(_lines.isEmpty())
+            _lines.add(str);
+        return _lines;
+    }
+}
