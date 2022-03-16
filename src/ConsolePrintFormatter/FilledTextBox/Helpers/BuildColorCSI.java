644790aeb4c703fc7c2094afb21c6a2b33e0eaa8
Cannabis2013 <MH2012@outlook.dk>
2022-03-16 18:04:12 +0100

    Refactored csi code

--- src/ConsolePrintFormatter/FilledTextBox/Helpers/BuildColorCSI.java
+++ src/ConsolePrintFormatter/FilledTextBox/Helpers/BuildColorCSI.java
@@ -4,12 +4,12 @@ import java.awt.*;
 
 public class BuildColorCSI {
     private String fgCode(Color rgb){
-        var csiComp = String.format("[38;2;%d;%d;%dm",rgb.getRed(),rgb.getGreen(),rgb.getBlue());
-        return (char) 27 + csiComp;
+        var csiComp = String.format("\033[38;2;%d;%d;%dm",rgb.getRed(),rgb.getGreen(),rgb.getBlue());
+        return csiComp;
     }
     private String bgCode(Color rgb){
-        var csiComp = String.format("[48;2;%d;%d;%dm",rgb.getRed(),rgb.getGreen(),rgb.getBlue());
-        return (char) 27 + csiComp;
+        var csiComp = String.format("\033[48;2;%d;%d;%dm",rgb.getRed(),rgb.getGreen(),rgb.getBlue());
+        return csiComp;
     }
     public String buildBg(Color color){
         var code = bgCode(color);
